c19ab6a510b7fe693be45709f258ac66cd6cfc68
Kevin Nguyen <kevintngu@hotmail.com>
2022-05-27 14:53:23 +1000

    switch pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2022_05_26_025232) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bookmarks", force: :cascade do |t|
     t.string "comment"
-    t.integer "movie_id", null: false
-    t.integer "list_id", null: false
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["list_id"], name: "index_bookmarks_on_list_id"
