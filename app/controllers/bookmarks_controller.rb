69f05c688625e24bd4a5388ee0a6b30b2b80a478
Kevin Nguyen <kevintngu@hotmail.com>
2022-05-27 12:14:51 +1000

    Adding root path

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -17,10 +17,10 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
-    @list = List.find(params[:list_id])
-    raise
-    @bookmark = Bookmark.find(params[])
-
+    # @list = List.find(params[:list_id])
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
   end
 
   private
